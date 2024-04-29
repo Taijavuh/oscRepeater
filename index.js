16c8ce3dd74854a8956666aae99eed7bbe43b58c
Andrew Lane <contact@andeh.co.uk>
2024-04-28 21:52:28 -0700

    Update to boilerplate

--- index.js
+++ index.js
@@ -2,8 +2,11 @@
 const osc = require("node-osc");
 const dynamics = require("./dynamics.json");
 
-let toRepeat = ["172.23.1.200", "172.23.1.218", "172.23.1.214"];
+let toRepeat = ["172.23.0.215", "172.23.1.235", "172.23.1.250"];
 let clients = [];
+let transition;
+
+let threads = [];
 
 for (let ip of toRepeat) {
   clients.push(new osc.Client(ip, 7000));
@@ -13,37 +16,86 @@ var oscServer = new osc.Server(7001, "0.0.0.0", () => {
   console.log("OSC Server is listening");
 });
 
+function executeSequence(sequence, value, index) {
+  for (let i = index || 0; i < sequence.length; i++) {
+    let item = sequence[i];
+    console.log(item);
+    try {
+      switch (item.type) {
+        case "osc_command":
+          sendOSC(item, value);
+          break;
+        case "pause":
+          console.log(`Pausing for ${item.duration} seconds`);
+          setTimeout(() => {
+            executeSequence(sequence, value, i + 1);
+          }, item.duration);
+          return;
+        default:
+          console.log(`Unknown type: ${sequence[item].type}`);
+          break;
+      }
+    } catch (e) {
+      console.error(e);
+    } // catch
+  } // for
+}
+
+function sendOSC(command, value) {
+  try {
+    let message = command.data;
+
+    if (value) {
+      console.log(`Value ${value}`);
+      message = message.replace("{value}", value);
+    } // if
+    send(command.uri, message);
+  } catch (e) {
+    console.log(e);
+  } // catch
+}
+
 function messageMatch(msg) {
   try {
-    let type = msg[0].split("/");
-    console.log(type);
-    if (type[1] == "setTeam") {
-      if (type[2] == "left") {
-        for (let bundle of dynamics.left) {
-          try {
-            if (bundle.value) {
-              send(bundle.uri, bundle.value);
-            } else {
-              send(bundle.uri, bundle.prefix + msg[1] + bundle.postfix);
-            }
-          } catch (e) {
-            console.log(e);
-          }
-        } // foreach
-      } else if (type[2] == "right") {
-        for (let bundle of dynamics.right) {
-          try {
-            if (bundle.value) {
-              send(bundle.uri, bundle.value);
-            } else {
-              send(bundle.uri, bundle.prefix + msg[1] + bundle.postfix);
-            } // else
-          } catch (e) {
-            console.log(e);
-          }
-        } // foreach
-      } // else if
+    let bundle = msg;
+    let uri = bundle.shift();
+    console.log(uri);
+    console.log(bundle);
+
+    if (uri == "/setTransition") {
+      if (bundle.length == 0) {
+        console.log(`Transition Cleared`);
+        return;
+      } // if
+      else {
+        for (let i = 0; i < Object.keys(dynamics).length; i++) {
+          let key = Object.keys(dynamics)[i];
+          if (dynamics[key].endpoint == bundle[0]) {
+            transition = dynamics[key];
+            console.log(`Transition set to ${transition.endpoint}`);
+          } // if
+        } // for
+      } // else
+      return;
     } // if
+    for (let i = 0; i < Object.keys(dynamics).length; i++) {
+      let key = Object.keys(dynamics)[i];
+      if (dynamics[key].endpoint == uri) {
+        console.log(`Matched: ${key}`);
+        for (let value in bundle) {
+          let sequence = dynamics[key].sequence;
+          if (dynamics[key].applyTransition && transition) {
+            let newArray = sequence;
+            for (let j = transition.sequence.length - 1; j >= 0; j--) {
+              newArray.unshift(transition.sequence[j]);
+              console.log(j);
+              sequence = newArray;
+            }
+          }
+          executeSequence(sequence, bundle[value]);
+        } // foreach
+      } // if
+    } // for
   } catch (e) {
     console.log(e);
   } // catch
